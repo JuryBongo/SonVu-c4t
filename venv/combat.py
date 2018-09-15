767e85f054bcf6255539e9c2d6f9d27c9b7c7994
C4T6 <chungta202@gmail.com>
2018-09-15 18:53:02 +0700

    BTVN Game

--- venv/combat.py
+++ venv/combat.py
@@ -1,100 +1,67 @@
 from random import randint
-steal_gaunlet={
-    "NAME":"STEAL GAUNLET",
-    "HP":10,
-    "AGI":-5,
-    "STA":-3,
-    "LUCK":1,
+from inventory import item_after_combat
 
 
-}
+def calculate_crit(attacker):
+    has_crit = False
+    dice = randint(0, 10)
+    chance = attacker["LUCK"] + attacker["AGI"]
+    if chance > dice:
+        has_crit = True
 
-bronze_shield = {
-    "NAME":"BRONZE SHIELD",
-    "HP":10,
-    "AGI":-2,
-}
+    return has_crit
 
-
-golden_stick = {
-    "NAME":"GOLDEN STICK",
-    "AGI":15,
-    "HP":20,
-    "STR":100,
-
-}
-org = {"NAME":"Org guard",
-        "STR": 1,
-                       "DEF": 2,
-                       "HP":69,
-                       "LUCK":1,
-                       "AGI":2,
-       }
-
-inventory=[]
 
 def combat_round(attacker, defender):
-    print(attacker["NAME"],"is beating",defender["NAME"])
+    print(attacker["NAME"], "đang tấn công", defender["NAME"])
     has_crit = calculate_crit(attacker)
-    if has_crit:
-        print("Chí mạng")
-        damage = attacker["STR"]*2 - defender["DEF"]
-    else:
 
-        damage = attacker["STR"]  - defender["DEF"]
+    if has_crit:
+        print("Đòn chí mạng")
+        damage = attacker["STR"] * 2 - defender["DEF"]
+    else:
+        damage = attacker["STR"] - defender["DEF"]
 
     if damage > 0:
         defender["HP"] -= damage
-        print(defender["NAME"], "lost", abs(damage), "HP")
-
-        print(defender["NAME"],"now has",defender["HP"],"HP")
+        print(defender["NAME"], "mất", damage, "HP")
+        print(defender["NAME"], "còn", defender["HP"], "HP")
     else:
         attacker["HP"] -= abs(damage)
-        print(attacker["NAME"], "lost", damage, "HP")
-        print(attacker["NAME"], "now has",attacker["HP"], "HP")
+        print(attacker["NAME"], "mất", abs(damage), "HP")
+        print(attacker["NAME"], "còn", attacker["HP"], "HP")
 
-def combat_full(player,opponent):
+
+def combat_full(player, opponent):
     while True:
-        auto_đánh = True
         combat_round(player, opponent)
-        if opponent["HP"] <= 0 or player["HP"]<=0:
-
+        if opponent["HP"] <= 0 or player["HP"] <= 0:
             break
-
-
-
-
-
 
         combat_round(opponent, player)
         if opponent["HP"] <= 0 or player["HP"] <= 0:
-
             break
-        if  auto_đánh:
-            print("Bạn muốn ?")
-            print("1.Đánh tiếp")
-            print("2.Chạy")
-            print("3.Auto đánh")
-            option = input("Your choice ?")
-            if option == 1:
-                pass
-            elif option == 2:
 
-                dice = randint(0, 100)
-                if player["LUCK"] > dice:
-                    print("Bạn đã chạy thoát")
-                    break
-                else:
-                    print("Return")
-            elif option == 3:
-                auto_đánh = False
+        print("Bạn muốn:")
+        print("1. Đánh tiếp")
+        print("2. Chạy")
+        print("3. Sử dụng đồ")
+        print("4. Tự động đánh")
+        option = input(">>> ")
+        if option == "1":
+            print("Nhào zô")
+        elif option == "2":
+            dice = randint(0, 100)
+            if player["LUCK"] > dice:
+                print("Bạn đã chạy thoát")
+                break
+            else:
+                print("Chạy trốn không thành công, bạn quay trở lại cuộc chiến")
+        elif option == "3":
+            print("Bài tập về nhà, hihi")
 
-
-
-def calculate_crit(player)   :
-    has_crit = False
-    dice = randint(0,100)
-    chance = player["LUCK"]+player["AGI"]
-    if chance > dice:
-        has_crit = True
-    return has_crit
+    if player["HP"] <= 0:
+        print("Bạn đã thua rồi, huhu")
+    else:
+        print(opponent["NAME"], "đã bị tiêu diệt")
+        item_after_combat()
--- venv/the_inventer.py
+++ venv/the_inventer.py
@@ -1,26 +1,112 @@
+from random import choice
+
+items = []
+
+def show_items(): #1. Vi tri, 2. ten item
+    # 1. Rau muống luộc cao cấp
+    # 2. Bánh bao chiên bình dân
+    pass
+
+
+def item_after_combat():
+    new_item = generate_item()
+    print("Một", new_item["NAME"], "vừa rơi ra")
+    while True:
+        print("1. Xem")
+        print("2. Nhặt")
+        print("3. Bỏ qua")
+        option = input(">>>")
+        if option == "1":
+            show_item(new_item)
+        elif option == "2":
+            add_item(new_item)
+            print("Bạn đã nhặt", new_item["NAME"], "vào hòm đồ")
+            count_items()
+            break
+        elif option == "3":
+            print("Bạn đã bỏ qua món đồ")
+            break
+
+def add_item(item):
+    items.append(item)
+
+
+def count_items():
+    count = len(items) # len ~ length
+    print("Bạn có", count, "đồ trong hòm")
+
+
+food_types = [
+    "Bánh bao",
+    "Cơm",
+    "Mỳ tôm",
+    "Trứng",
+    "Rau muống",
+]
+
+cook_types = [
+    "hấp",
+    "chiên",
+    "luộc"
+]
+
+food_levels = [
+    "bình dân",
+    "cao cấp",
+    "xa xỉ"
+]
+
+
+def generate_item_name():
+    ft = choice(food_types)
+    ct = choice(cook_types)
+    fl = choice(food_levels)
+    item_name = ft + " " + ct + " " + fl
+    return item_name
+
+
+def generate_item():
+    name = generate_item_name()
+    item = {
+        "NAME": name,
+        "AGI": 3,
+        "HP": -1,
+        "DEF": 2,
+        "STR": 2,
+    }
+    return item
+
+
 def show_item(game_item):
+    print("* " * 15)
+
     for key, value in game_item.items():
-        print( key, ":", value)
+        print("*", key, ":", value)
 
+    print("* " * 15)
 
-
-steel_gaunlet = {
-    "NAME": "Steel gaunlet",
-    "HP": 10,
-    "STR": 100,
-    "AGI": 5,
-}
-bronze_shield = {
-    "NAME": "Bronze shield",
-    "HP": 5,
-    "AGI": 1,
-    "STR": 100,
-}
-golden_stick = {
-    "NAME": "Golden stick",
-    "AGI":15,
-    "HP":20,
-    "STR": 100,
-}
-
-inventory = [steel_gaunlet, bronze_shield, golden_stick]
+#
+#
+# steel_gauntlet = {
+#     "NAME": "STEEL GAUNLET",
+#     "HP": 10,
+#     "AGI": 5,
+#     "LUCK": 1,
+# }
+#
+# bronze_shield = {
+#     "NAME": "BRONZE SHIElD",
+#     "HP": 5,
+#     "AGI": 1,
+# }
+#
+# golden_stick = {
+#     "NAME": "GOLDEN STICK",
+#     "AGI": 15,
+#     "HP": 20,
+#     "STR": 100,
+# }
+#
+# inventory = [steel_gaunlet, bronze_shield, golden_stick]
+# for item in inventory:
+#     show_item(item)
